4f3c8d6cde82f67b45c64be4fa470c9e9fa6cba9
krindioom <47291437+krindioom@users.noreply.github.com>
2024-02-07 18:17:44 +0700

    добавил некоторые спа-компоненты

--- front/intertech/src/App.jsx
+++ front/intertech/src/App.jsx
@@ -1,15 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { RecoilRoot } from "recoil"
 import './App.css'
+import { Route, Routes } from "react-router-dom/dist"
+import MainLayout from "./Components/Layouts/MainLayout"
+import MainPage from "./Components/Main/MainPage"
+
 
 function App() {
 
     return (
-        <>
-            <div>
-            </div>
-        </>
+        <RecoilRoot>
+            <Routes>
+                <Route path="/" element={<MainLayout />}>
+                    <Route index element={<MainPage/>}/>
+                </Route>
+            </Routes>
+        </RecoilRoot>
     )
 }
 
